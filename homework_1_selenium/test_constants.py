b2cf5effbed8a8ba09c05ba19cd110739097c8f6
Renat Devetyarov <renat.devetyarov@kaspersky.com>
2021-10-16 22:36:17 +0300

    Add constants for tests

---
+++ homework_1_selenium/test_constants.py
@@ -0,0 +1,5 @@
+class TestConstants:
+    PATH_TO_DRIER_CHROME = "/home/renatdevetyarov/drivers/chromedriver"
+    URL_TEST = "https://target.my.com"
+    VALID_LOGIN = "ivinov.ivan@bk.ru"
+    VALID_PASSWORD = "SimplePassword123#"
