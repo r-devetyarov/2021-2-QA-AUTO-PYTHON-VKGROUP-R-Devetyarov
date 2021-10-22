8ec501b2cf4d7520394111bb78faa652c9f12341
Renat Devetyarov <renat.devetyarov@kaspersky.com>
2021-10-22 22:28:11 +0300

    Refactor PR review: - fix grammar mistake

--- homework_1_selenium/test_constants.py
+++ homework_1_selenium/test_constants.py
@@ -1,5 +1,5 @@
 class TestConstants:
-    PATH_TO_DRIER_CHROME = "/home/renatdevetyarov/drivers/chromedriver"
+    PATH_TO_DRIVER_CHROME = "/home/renatdevetyarov/drivers/chromedriver"
     URL_TEST = "https://target.my.com"
     VALID_LOGIN = "ivinov.ivan@bk.ru"
     VALID_PASSWORD = "SimplePassword123#"
