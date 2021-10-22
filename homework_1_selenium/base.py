0c06702c4032ce70035946c17ff635fe8f06f3c2
Renat Devetyarov <renat.devetyarov@kaspersky.com>
2021-10-22 22:24:51 +0300

    Refactor PR review: down timeout, count retry for waiting, add work with timeout exceptions

--- homework_1_selenium/base.py
+++ homework_1_selenium/base.py
@@ -1,13 +1,16 @@
-import time
 from typing import Tuple
 
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-CLICK_RETRY = 10
-TIMEOUT = 20
+CLICK_RETRY = 3
+TIMEOUT = 5
+
+
+class WebDriverWaitingException(Exception):
+    pass
 
 
 class Base:
@@ -15,27 +18,29 @@ class Base:
         self.driver = driver
 
     def find(self, locator: Tuple[By, str]):
-        WebDriverWait(self.driver, TIMEOUT).until(
-            EC.presence_of_element_located(locator)
-        )
-        return self.driver.find_element(*locator)
+        try:
+            self.wait().until(EC.presence_of_element_located(locator))
+            return self.driver.find_element(*locator)
+        except TimeoutException:
+            raise WebDriverWaitingException(f"Element {locator[1]} not presence after {TIMEOUT}sec")
 
     def click_by(self, locator: Tuple[By, str]) -> None:
-        self.wait_for_clickable(locator)
         for i in range(CLICK_RETRY):
-            element = self.find(locator)
             try:
-                element.click()
+                self.find(locator)
+                elem = self.wait().until(EC.element_to_be_clickable(locator))
+                elem.click()
                 return
             except StaleElementReferenceException:
-                time.sleep(0.5)
-                if i == CLICK_RETRY:
+                if i == CLICK_RETRY - 1:
                     raise
-
-    def wait_for_clickable(self, locator: Tuple[By, str], timeout=TIMEOUT):
-        WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+            except TimeoutException:
+                raise WebDriverWaitingException(f"Element {locator[1]} not clickable after {TIMEOUT}sec")
 
     def clear_and_send_keys(self, locator: Tuple[By, str], text: str):
         locator = self.find(locator)
         locator.clear()
         locator.send_keys(text)
+
+    def wait(self, timeout=TIMEOUT):
+        return WebDriverWait(self.driver, timeout=timeout)
