eadfcd5e2bc1254c53475c5c12e302bfd5e71f58
Renat Devetyarov <renat.devetyarov@kaspersky.com>
2021-11-07 20:52:24 +0300

    constants for tests

---
+++ homework_3_api/constants.py
@@ -0,0 +1,6 @@
+class Constants:
+    PATH_TO_DRIVER_CHROME = "/home/renatdevetyarov/drivers/chromedriver"
+    URL_TEST = "https://target.my.com"
+    VALID_LOGIN = "ivinov.ivan@bk.ru"
+    VALID_PASSWORD = "SimplePassword123#"
+    SELENOID_ADDRESS = "http://127.0.0.1:4444/wd/hub"
